0db2d52ca4bc310eaa7197403dbf8569077b261c
euronion <42553970+euronion@users.noreply.github.com>
2021-08-09 16:20:07 +0200

    Create download_weather_data.jl

---
+++ workflow/scripts/download_weather_data.jl
@@ -0,0 +1,9 @@
+# Activate julia environment
+using Pkg
+Pkg.activate("./workflow/envs")
+using GlobalEnergyGIS
+
+era_year = parse(Int64, snakemake.wildcards["era_year"])
+
+println("Downloading ERA5 data.")
+download_and_convert_era5(era_year)
